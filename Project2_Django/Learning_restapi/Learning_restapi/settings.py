6764a1bbe94f96df0a8848adf513a520efbf2aa0
priyankapriyadarshini50 <90296756+priyankapriyadarshini50@users.noreply.github.com>
2021-09-30 14:35:47 +1000

    Update settings.py

--- Project2_Django/Learning_restapi/Learning_restapi/settings.py
+++ Project2_Django/Learning_restapi/Learning_restapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7&tr9$$uu&pd1_faskl6p1g25x@3n=ybo4d**(aczi2$z88u90'
+SECRET_KEY = 'dja'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
