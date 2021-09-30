c216056ffa0cdb91cfcb5283ac303264ec724117
priyankapriyadarshini50 <90296756+priyankapriyadarshini50@users.noreply.github.com>
2021-09-30 20:01:53 +1000

    Delete Project2_Django/Django_MongoDB_api directory

--- Project2_Django/Django_MongoDB_api/basic_api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from basic_api.models import Tutorial
-
-
-class TutorialSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tutorial
-        fields = "__all__"
